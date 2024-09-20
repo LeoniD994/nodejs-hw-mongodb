a6c026a2df4621ac149c6aaee522eb78b589d1a3
LeoniD994 <leon-zls@live.com>
2024-09-20 21:02:24 +0300

    update errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,8 +4,8 @@ const errorHandler = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).json({
       status: err.status,
-      message: err.message || 'Error',
-      data: err.data || null,
+      message: err.name,
+      data: err,
     });
     return;
   }
